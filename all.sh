678020cd4828dd31d9c48dfbdcdd91f3fe6cfe1f
Muhammad Mufid <mufidu@outlook.com>
2021-09-22 18:53:51 +0700

    Update path

--- all.sh
+++ all.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-konsole --noclose -e $HOME/repos/script/blessed.sh & sleep 1s && konsole --noclose -e $HOME/repos/script/htop.sh & sleep 2s && konsole --noclose -e $HOME/repos/script/neofetch.sh
+konsole --noclose -e /media/mufid/Files/repos/script/blessed.sh & sleep 1s && konsole --noclose -e /media/mufid/Files/repos/script/htop.sh & sleep 2s && konsole --noclose -e /media/mufid/Files/repos/script/neofetch.sh
