21831db984af430a328d21b3d379eccdcbebf2e1
mufidu <mufid.to@gmail.com>
2024-07-08 18:31:00 +0700

    Update harddisk path

--- mac/rsync/reddit-harddisk.sh
+++ mac/rsync/reddit-harddisk.sh
@@ -5,4 +5,4 @@ PATH=/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin
 
 rclone copy -M -P "/Users/mufid/repos/Reddit" "personal:PRIVATE/BACKUP/Reddit" --exclude=".DS_Store" --exclude=".Trash/" -vv
 
-rclone copy -M -P "/Users/mufid/repos/Reddit" "/Users/mufid/.mounty/Files/PRIVATE/BACKUP/Reddit" --exclude=".DS_Store" --exclude=".Trash/" -vv
+rclone copy -M -P "/Users/mufid/repos/Reddit" "/Volumes/HDD/PRIVATE/BACKUP/Reddit" --exclude=".DS_Store" --exclude=".Trash/" -vv
